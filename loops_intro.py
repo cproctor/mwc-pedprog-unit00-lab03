db221ab13c6d1960ff5fa941ebb466735780279e
Chris Proctor <chris@chrisproctor.net>
2022-01-27 18:07:12 -0500

    Initial commit

---
+++ loops_intro.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+# [PART A: CALCULATIONS]
+print("-- Calculations --")
+for i in range(10):
+    print("looping...")
+    print(i)
+
+# [PART B: SQUARING NUMBERS]
+# YOUR CODE GOES HERE
+
+# [PART C: LOOPING A SQUARE]
+# YOUR CODE GOES HERE
